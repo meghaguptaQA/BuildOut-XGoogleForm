4f5545fa98610872edf006f44ef05d7bc52b9286
meghaguptaQA <meghagupta22@gmail.com>
2025-02-11 10:14:50 +0530

    Update Wrappers.java

--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,11 +1,14 @@
 package demo.wrappers;
 
+import demo.TestCases;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,7 +16,61 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Wrappers {
+
     /*
      * Write your selenium wrappers here
      */
+    public static void openURL(WebDriver driver,String browserURL) {
+
+		System.out.println("Maximize the window");
+		driver.manage().window().maximize();
+
+		System.out.println("Opening Website -->" + browserURL);
+		driver.get(browserURL);
+	}
+    public static String GetCurrentURL(WebDriver driver)
+    {   
+        try {       
+             String url=driver.getCurrentUrl();
+             return url;
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+                return null;
+
+    }
+    public static void enter_Value(WebElement element,String attributeValue)
+    {
+        try 
+        {
+            element.clear();
+            element.sendKeys(attributeValue);
+        } catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public static void btn_click(WebElement element)
+    {
+        try 
+        {
+            element.click();
+        } catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String date_decrmenter(LocalDate todaysdate,Integer days )
+    {
+        
+        LocalDate date7daysbefore=todaysdate.minusDays(7);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/YYYY");
+        String datetoenter = date7daysbefore.format(format);
+        return datetoenter;
+    }
 }
