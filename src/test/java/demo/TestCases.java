e44ff705f4fad04d6b9c2f1e96d2a07d72fa1e58
meghaguptaQA <meghagupta22@gmail.com>
2025-02-11 10:14:21 +0530

    Update TestCases.java

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,7 @@
 package demo;
-
+import demo.wrappers.Wrappers;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -13,6 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.sql.Wrapper;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -25,22 +27,11 @@ import demo.wrappers.Wrappers;
 public class TestCases {
     ChromeDriver driver;
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
-
-     
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
     @BeforeTest
     public void startBrowser()
     {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
@@ -68,92 +59,73 @@ public class TestCases {
     public void testCase01() throws InterruptedException {
         System.out.println("Start Test case: testCase01");
         //Open the URL to the forms page
-        openURL("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
-        String url=GetCurrentURL();
+        Wrappers.openURL(driver,"https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        String url=Wrappers.GetCurrentURL(driver);
         Thread.sleep(5000);
-        //System.out.println("Current URL of the form page :"+url);
+   
         
-        //Enter the value "Crio Learner" in Name field
-        //Xpath : //div[@class='rFrNMe k3kHxc RdH0ib yqQS1 zKHdkd']
-        WebElement txtboxName=driver.findElement(By.xpath("//input[@aria-labelledby='i1 i4']"));
-        enter_Value(txtboxName,"Crio Learner");
+        //Enter the value "Crio Learner" in Name field  
+        WebElement txtboxName=driver.findElement(By.xpath("//input[@type='text']"));
+        Wrappers.enter_Value(txtboxName,"Crio Learner");
         //Get current epoch time
         long epochtime=System.currentTimeMillis()/1000;
         //Enter value "I want to be the best QA Engineer! +current epoch time "in Why are you practicing Automation? field
         //Xpath : //div[@class='edhGSc zKHdkd kRy7qc RdH0ib yqQS1']
         WebElement txtboxPracticeAutomation=driver.findElement(By.xpath("//textarea[@class='KHxj8b tL9Q4c']"));
-        enter_Value(txtboxPracticeAutomation,"I want to be the best QA Engineer! "+epochtime);
+        Wrappers.enter_Value(txtboxPracticeAutomation,"I want to be the best QA Engineer! "+epochtime);
         //Select value in Experience Radio button
-        WebElement radiobtnExperience=driver.findElement(By.id("i25"));
-        btn_click(radiobtnExperience);
-        //Select Java Checkbox in Which of the following have you learned in Crio.Do for Automation Testing?
-        WebElement checkboxJava=driver.findElement(By.id("i34"));
-        btn_click(checkboxJava);
+  
+        String txtExp="0 - 2";
+        WebElement radiobtnExperience=driver.findElement(By.xpath("//span[contains(@class,'OvPDhc') and contains(text(),'"+txtExp+"')]"));
+        Wrappers.btn_click(radiobtnExperience);
+
+        String txtJava="Java";
+        String txtSelenium="Selenium";
+        String txtTestNG="TestNG";
+        WebElement checkboxJava=driver.findElement(By.xpath("//span[contains(text(),'"+txtJava+"')]/../../..//div[2]"));
+        Wrappers.btn_click(checkboxJava);
         //Select Selenium Checkbox in Which of the following have you learned in Crio.Do for Automation Testing?
-        WebElement checkboxSelenium=driver.findElement(By.id("i37"));
-        btn_click(checkboxSelenium);
+        WebElement checkboxSelenium=driver.findElement(By.xpath("//span[contains(text(),'"+txtSelenium+"')]/../../..//div[2]"));
+        Wrappers.btn_click(checkboxSelenium);
         //Select TestNG Checkbox in Which of the following have you learned in Crio.Do for Automation Testing?
-        WebElement checkboxTestNG=driver.findElement(By.id("i43"));
-        btn_click(checkboxTestNG);
+        WebElement checkboxTestNG=driver.findElement(By.xpath("//span[contains(text(),'"+txtTestNG+"')]/../../..//div[2]"));
+        Wrappers.btn_click(checkboxTestNG);
 
         WebElement dropdown = driver.findElement(By.xpath("//div[contains(@class,'MocG8c')]"));
         //System.out.println("Selected preferred address type");
 
         // Click to open the dropdown
-        dropdown.click();
+        Wrappers.btn_click(dropdown);
 
         Thread.sleep(3000);
-       
-        WebElement option = driver.findElement(By.xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div[2]/div[3]/span"));
-        option.click();
-       
-        //Select value in How should you be addressed? dropdown //div//span[text()='Mr']/./preceding-sibling::div[@class='kRoyt MbhUzd']
-        //WebElement dropdwnAddressedAs=driver.findElement(By.xpath("//span[text()='Choose']"));
-        //dropdwnAddressedAs.click();
-        //WebElement dropdownValues=driver.findElement(By.xpath("//span[text()='Choose']/parent::div[1]"));
-       // dropdownValueMs.click();
-
-        // List<WebElement> dropdownValues=driver.findElements(By.xpath("//div[@class='MocG8c HZ3kWc mhLiyf OIC90c LMgvRb']"));
-		// System.out.println("size is"+dropdownValues.size());
-		// // TODO : Create a Javascript executor to click on particular value
-		// JavascriptExecutor js=(JavascriptExecutor) driver;		
-		// // TODO : Iterate on each value using for each loop and if matching desired value is
-		// // found, perform click() operation
-		// for(WebElement chooseval:dropdownValues)
-		// {   System.out.println("data-value is : "+chooseval.getAttribute("data-value"));
-		// 	if(chooseval.getAttribute("data-value").equalsIgnoreCase("Dr"))
-		// 	{   WebElement valuetoclick=chooseval.findElement(By.xpath("//span[text()='Dr']"));
-		// 		js.executeScript("arguments[0].scrollIntoView();",valuetoclick);
-		// 		chooseval.click();
-		// 		break;
-		// 	}
-		// }
+        //Choose value in Addressed dropdown
+       String txtMs="Ms";
+       List<WebElement> options=driver.findElements(By.xpath("//div[contains(@class,'ncFHed')]//span[not(contains(text(),'Choose'))]"));
+       for (WebElement option : options)
+       {
+            if(option.getText().contains(txtMs))
+            {
+                Wrappers.btn_click(option);
+            }
+       }
         //Enter the date 7 days ago in field:What was the date 7 days ago?
         //Calculate the date
         LocalDate todaysdate=LocalDate.now();
-        LocalDate date7daysbefore=todaysdate.minusDays(7);
-        //System.out.println(date7daysbefore);
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/YYYY");
-        String datetoenter = date7daysbefore.format(format);
-        //System.out.println(datetoenter);
-        WebElement txtboxDate=driver.findElement(By.xpath("//input[@type='date']"));        
-        enter_Value(txtboxDate,datetoenter);
+        String datetoenter=Wrappers.date_decrmenter(todaysdate,7);      
 
-        //Enter the time now field in What is the time right now?
-        LocalTime timenow=LocalTime.now();
-        int hour=timenow.getHour();
-        int minute=timenow.getMinute();
-        String hours=String.valueOf(hour);
-        String minutes=String.valueOf(minute);
+        System.out.println(datetoenter);
+        WebElement txtboxDate=driver.findElement(By.xpath("//input[@type='date']"));        
+        Wrappers.enter_Value(txtboxDate,datetoenter);
+
         
         WebElement txthour=driver.findElement(By.xpath("//input[@aria-label='Hour']"));
-        enter_Value(txthour, "07");
+        Wrappers.enter_Value(txthour, "07");
         WebElement txtminute=driver.findElement(By.xpath("//input[@aria-label='Minute']"));
-        enter_Value(txtminute, "30");
+        Wrappers.enter_Value(txtminute, "30");
 
         //Click on Submit button
         WebElement btnSubmit=driver.findElement(By.xpath("//span[text()='Submit']"));
-        btn_click(btnSubmit);
+        Wrappers.btn_click(btnSubmit);
         Duration timeout=Duration.ofMillis(10000);
         WebDriverWait wait = new WebDriverWait(driver, timeout);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Thanks for your response, Automation Wizard!']")));
@@ -174,26 +146,5 @@ public class TestCases {
         System.out.println("end Test case: testCase01");
         
     }
-    public void openURL(String browserURL) {
-
-		System.out.println("Maximize the window");
-		driver.manage().window().maximize();
-
-		System.out.println("Opening Website -->" + browserURL);
-		driver.get(browserURL);
-	}
-    public String GetCurrentURL()
-    {
-        String url=driver.getCurrentUrl();
-        return url;
-    }
-    public void enter_Value(WebElement element,String attributeValue)
-    {
-        element.sendKeys(attributeValue);
-    }
-    
-    public void btn_click(WebElement element)
-    {
-        element.click();
-    }
+   
 }
